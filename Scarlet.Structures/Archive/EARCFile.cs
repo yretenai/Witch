b78eb211e072445c5ce6ec89b3781477a9de28dd
Naomi Ahmed <yretenai@gmail.com>
2023-01-28 20:25:03 +0000

    Unshuffle these fields due to code cleanup

--- Scarlet.Structures/Archive/EARCFile.cs
+++ Scarlet.Structures/Archive/EARCFile.cs
@@ -5,16 +5,16 @@ namespace Scarlet.Structures.Archive;
 
 [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 0x28)]
 public record struct EARCFile {
-    public int ArchivePathOffset;
     public ulong Checksum;
-    public int CompressedSize;
-    public long DataOffset;
-    public EARCFileFlags Flags;
-    public ushort Key;
-    public byte Locale;
-    public int PathOffset;
     public int Size;
+    public int CompressedSize;
+    public EARCFileFlags Flags;
+    public int PathOffset;
+    public long DataOffset;
+    public int ArchivePathOffset;
     public byte Type;
+    public byte Locale;
+    public ushort Key;
 
     public string GetPath(MemoryOwner<byte> buffer) => MemoryHelper.GetString(buffer, PathOffset);
     public string GetArchivePath(MemoryOwner<byte> buffer) => MemoryHelper.GetString(buffer, ArchivePathOffset);
--- Scarlet.Structures/Archive/EARCHeader.cs
+++ Scarlet.Structures/Archive/EARCHeader.cs
@@ -7,16 +7,16 @@ public record struct EARCHeader {
     public const uint MagicValue = 0x46415243; // FARC - File Archive
     public const ulong ChecksumXOR1 = 0xCBF29CE484222325;
     public const ulong ChecksumXOR2 = 0x8B265046EDA33E8A;
-    public uint ANTOffset;
-    public uint BlockSize;
-    public ulong Checksum;
-    public uint ChunkSize;
-    public uint DataOffset;
-    public uint FATOffset;
-    public uint FileCount;
-    public EARCFlags Flags;
-    public uint FNTOffset;
 
     public uint Magic;
     public int Version;
+    public uint FileCount;
+    public uint BlockSize;
+    public uint FATOffset;
+    public uint FNTOffset;
+    public uint ANTOffset;
+    public uint DataOffset;
+    public EARCFlags Flags;
+    public uint ChunkSize;
+    public ulong Checksum;
 }
