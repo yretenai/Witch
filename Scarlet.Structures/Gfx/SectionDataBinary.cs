3e2681a0345c7a82f26bdf232ce50209fcd3a962
Naomi Ahmed <yretenai@gmail.com>
2023-02-02 00:23:03 +0000

    SEDB stuff

---
+++ Scarlet.Structures/Gfx/SectionDataBinary.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Scarlet.Structures.Gfx;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public record struct SectionDataBinary {
+    public uint Magic;
+    public uint Type;
+    public int Count;
+    public ushort Reserved;
+    public ushort DataOffset;
+    public uint FileSize;
+}
