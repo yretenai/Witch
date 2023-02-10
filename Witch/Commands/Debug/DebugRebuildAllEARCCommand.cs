65115f28b0b697bc05df81e989637a31daf3f418
Naomi Ahmed <yretenai@gmail.com>
2023-02-10 06:34:08 +0000

    debug rebuild-all

---
+++ Witch/Commands/Debug/DebugRebuildAllEARCCommand.cs
@@ -0,0 +1,18 @@
+﻿using DragonLib.CommandLine;
+using Scarlet;
+using Scarlet.Archive;
+using Serilog;
+
+namespace Witch.Commands.Debug;
+
+[Command(typeof(WitchFlags), "rebuild-all", "", "debug", true)]
+public class DebugRebuildAllEARCCommand : EARCCommand {
+    public DebugRebuildAllEARCCommand(WitchFlags flags) : base(flags) {
+        foreach (var (_, earc) in AssetManager.Instance.Archives) {
+            var builder = new EbonyArchiveBuilder(earc);
+            Log.Information("Rebuilding {EARC}", earc.DataPath);
+            using var output = new FileStream(Path.GetFileNameWithoutExtension(earc.DataPath) + ".earc", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+            builder.Build(output);
+        }
+    }
+}
